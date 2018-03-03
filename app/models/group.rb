168461b64d249ac48b1b8a05cf355f907bef02a0
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-03 21:17:36 +0900

    annotateの実行とgemfileの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Group < ApplicationRecord
   has_many :messages
   has_many :group_users

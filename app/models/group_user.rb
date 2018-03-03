fcd8df956f6f4cdee5e30aed8dca50af564fdee7
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-03 22:06:50 +0900

    #2annotateの実行とmodelの修正

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: group_users
+#
+#  id         :integer          not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class GroupUser < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  validates :user_id, :group_id, numericality: true, presence: true
+  validates :user_id, :group_id, numericality: { only_integer: true }, presence: true
 end

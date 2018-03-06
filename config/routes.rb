cf0daa68e0614b875352ea17e9009eff4934416b
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-06 15:37:49 +0900

    グループ作成画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: %i(edit update)
+  resources :groups, only: %i(new create edit update) do
+    resources :messages, only: [:index, :create]
+  end
 
 end
 

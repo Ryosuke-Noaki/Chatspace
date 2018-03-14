da9b9c2465dc1cb15f55ef6e9e2813a9732e7f07
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-14 19:45:20 +0900

    app/models/group.rb
    エラーメッセージのメソッドを追加した

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,32 @@
 class GroupsController < ApplicationController
+
+
   def index
   end
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
 
-def create
+  def create
     @group = Group.new(name: group_params[:name])
     if @group.save
-      group_params[:user_ids].each do |user_id|
-        @group.users << User.find(user_id)
-      end
-      flash.now[:notice] = 'グループ作成成功'
-      redirect_to root_path
+      save_groups_associated_in_user_id(group: @group)
+      redirect_to root_path, notice: 'グループ作成成功'
     else
       flash.now[:alert] = 'グループ作成失敗'
-      redirect_to new_group_path
+      render :new
     end
   end
 
   private
+
+  def save_groups_associated_in_user_id(group:)
+    group_params[:user_ids].each do |user_id|
+      group.users << User.find(user_id)
+    end
+  end
+
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -12,7 +12,19 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
-  accepts_nested_attributes_for :users
 
   validates :name, presence: true, uniqueness: true
+
+
+  def count_error_message
+    self.errors.full_messages.count
+  end
+
+  def add_error_message
+    self.errors.full_messages
+  end
+
 end
+
+
+

856c853f36b80d212b78871bbb15370d7d89198c
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-15 18:03:52 +0900

    メソッドの記述を修正した

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(name: group_params[:name])
     if @group.save
-      save_groups_associated_in_user_id(group: @group)
+      @group.associate_users(user_ids: group_params[:user_ids])
       redirect_to root_path, notice: 'グループ作成成功'
     else
       flash.now[:alert] = 'グループ作成失敗'
@@ -20,12 +20,6 @@ class GroupsController < ApplicationController
   end
 
   private
-
-  def save_groups_associated_in_user_id(group:)
-    group_params[:user_ids].each do |user_id|
-      group.users << User.find(user_id)
-    end
-  end
 
   def group_params
     params.require(:group).permit(:name, user_ids: [])

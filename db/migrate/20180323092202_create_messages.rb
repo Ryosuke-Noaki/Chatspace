af76b2994ffc639437d4961787e1e400f2d93e76
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-23 19:54:36 +0900

    imageにnot nullとdefault を追加した

--- db/migrate/20180323092202_create_messages.rb
+++ db/migrate/20180323092202_create_messages.rb
@@ -2,7 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :body
-      t.string :image
+      t.string :image, null: false, default: ""
+
       t.references :group, null: false, foreign_key: true, index: true
       t.references :user, null: false, foreign_key: true, index: true
       t.timestamps null: false

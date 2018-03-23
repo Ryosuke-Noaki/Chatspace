d18667f04d748e39b671bc0f54d9fdb2dc137824
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-23 11:01:28 +0900

    いらない記述を消去した

--- app/decorators/group_decorator.rb
+++ app/decorators/group_decorator.rb
@@ -10,13 +10,4 @@ class GroupDecorator < Draper::Decorator
     self.error_messages.count
   end
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
 end

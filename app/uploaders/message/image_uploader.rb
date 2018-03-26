77bbada8499cca55a40fa9549706dc71592ad58f
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-03-26 15:27:07 +0900

    uploader下にmessageディレクトリを作成し、そこにimage_uploader.rbを入れた。whitelistを追加し、アップロードを許可する拡張子を記述した。

--- app/uploaders/message/image_uploader.rb
+++ app/uploaders/message/image_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class Message::ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
@@ -6,9 +6,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
 
 end

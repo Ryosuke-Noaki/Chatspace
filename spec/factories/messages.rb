8229893829a7b15da5942db903bae6deed320d02
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-04-15 13:23:38 +0900

    annotateでテーブルのスキーマを追加し、アソシエーション部分の記述を修正した。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,21 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  body       :text(65535)
+#  image      :string(255)      default(""), not null
+#  group_id   :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :message do
-    content Faker::Lorem.sentence
+    body Faker::Lorem.sentence
     image File.open("#{Rails.root}/public/images/no_image.jpg")
-    user
-    group
+    associate :user, factory: :user
+    associate :group, factory: :group
   end
 end

a018a0db09fe1a45a2e1892c1563dd1c816c0bfc
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-05-23 09:16:07 +0900

    associate_usersのメソッドをテストする用のuserを作成した

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -20,9 +20,14 @@
 
 FactoryBot.define do
   factory :user do
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    sequence :name do |n|
+      "test_name#{n}"
+    end
+
     password = Faker::Internet.password
-    name Faker::Name.last_name
-    email Faker::Internet.free_email
     password password
     password_confirmation password
   end

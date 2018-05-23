0e9ebfb62560c61650d266ca5d6247dff162733d
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-05-23 09:18:00 +0900

    uniquenessとassociate_usersのテストを作成した。nameの保存が成功するテストで、expect文を追加した。nameの保存が失敗するテストの内容を修正した。

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -12,31 +12,47 @@ require 'rails_helper'
 
 RSpec.describe Group,type: :model do
   describe '#create' do
+    let(:group) { build(:group) }
     context 'can save' do
-      let(:group) { build(:group) }
       it 'is valid with name' do
         group = build(:group, name: Faker::Team.name)
+        expect(group).to be_valid
+      end
+
+      it 'confirms prepared users' do
+        group = build(:group)
+        users = create_list(:user, 10)
+        user_ids = users.map {|user| user.id.to_i}
+        group.associate_users(user_ids: user_ids)
+        expect(group.users).to eq users
       end
     end
 
     context 'can not save' do
-      it 'is invalid with name' do
-        group = build(:group, name: nil)
+      it 'is invalid without name' do
+        group.name = nil
         group.valid?
         expect(group.errors[:name]).to include('を入力してください')
       end
 
-      it 'is invalid with name' do
+      it 'is invalid without name' do
         group = build(:group, name: "")
         group.valid?
         expect(group.errors[:name]).to include('を入力してください')
+      end
+
+      it 'is invalid with same name' do
+        name = "test_group"
+        create(:group, name: name)
+        group = build(:group, name: name)
+        group.valid?
+        expect(group.errors[:name]).to include('はすでに存在します')
       end
     end
   end
 
   describe '#update' do
     let(:group) { create(:group) }
-
     context 'can save' do
       it 'is valid with name' do
         expect(group.update(name: "テスト")).to be true

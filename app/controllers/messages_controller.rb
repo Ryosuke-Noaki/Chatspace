ed6b9d657544b82c6a33dec57f3b5f5a6ad37a9e
Ryosuke-Noaki <ryosuke_wsd611@akane.waseda.jp>
2018-06-24 19:57:43 +0900

    message#createでhtmlとjsonに処理を分けるようにした

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,16 @@ class MessagesController < ApplicationController
     @current_user_groups = GroupDecorator.decorate_collection(current_user_groups)
   end
 
+
   def create
     @message = @group.messages.new(message_params)
     @message.user = current_user
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.html {redirect_to group_messages_path(@group)}
+        format.json
+      end
+      flash[:notice] = 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
